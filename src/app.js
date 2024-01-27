5a4e447414311674e9fa1dfe17758ee0fa9c38db
oxangyal <Oxana.mitch@gmail.com>
2024-01-26 23:56:34 -0500

    Connected to the data base

--- src/app.js
+++ src/app.js
@@ -3,28 +3,33 @@ const app = express();
 
 require("dotenv").config();
 
-
 const cors = require("cors");
-
 
 app.use(express.static("public"));
 
+//connectDB
+const connectDB = require("./db/connect");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-
-
 app.use(express.json());
-
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 6000;
 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
+    } catch (error) {
+        console.log(error);
+    }
+};
 
+start();
