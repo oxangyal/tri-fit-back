53c79cff63a6ec227483ee59b8355bbd7ac04264
oxangyal <Oxana.mitch@gmail.com>
2024-01-26 23:49:55 -0500

    Implemented error handeling

--- src/app.js
+++ src/app.js
@@ -3,14 +3,28 @@ const app = express();
 
 require("dotenv").config();
 
+
 const cors = require("cors");
 
 
 app.use(express.static("public"));
 
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+
+
 app.use(express.json());
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 6000;
 
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
+
---
+++ src/middleware/error-handler.js
@@ -0,0 +1,37 @@
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const errorHandlerMiddleware = (err, req, res, next) => {
+    let customError = {
+        statusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || "Something went wrong, please try again later",
+    };
+    // if (err instanceof CustomAPIError) {
+    //   return res.status(err.statusCode).json({ msg: err.message})
+    // }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    if (err.name === "ValidationError") {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(",");
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(
+            err.keyValue
+        )} field, please choose another value`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+
+    if (err.name === "CastError") {
+        console.log(err);
+        customError.msg = `No item was found with id: ${err.value}`;
+        customError.statusCode = StatusCodes.NOT_FOUND;
+    }
+
+    return res
+        .status(customError.statusCode)
+        .json({ msg: customError.msg, err });
+};
+
+module.exports = errorHandlerMiddleware;
